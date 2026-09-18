7535efcc0d089b50d4526e5a7a41972a51f4d91f
p <bmadd.rdi@gmail.com>
2026-09-18 13:48:50 +0700

    edit Tab on KB User to Pass

--- frontend/src/features/auth/components/login-form.tsx
+++ frontend/src/features/auth/components/login-form.tsx
@@ -136,7 +136,7 @@ export function LoginForm() {
             >
               Password
             </Label>
-            <Link href="/forgot-password">
+            <Link href="/forgot-password" tabIndex={-1}>
               <p className="text-sm font-medium text-primary transition-colors hover:underline hover:text-primary/80 whitespace-nowrap">
                 ลืมรหัสผ่าน?
               </p>
